c793281e2c4d9242fe6d1f7b7f052d80b4e6ee3c
sevenfate <7fate@web.de>
2016-09-28 12:00:33 +0200

    Small Fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,6 @@ export interface HeaderProps { compiler: string; framework: string; }
 
 export class Header extends React.Component<HeaderProps, {}> {
     render() {
-        return <h1>Wellcome to E2OPEn Notification Center build with {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Welcome to E2OPEn Notification Center build with {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
